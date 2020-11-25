bb7108eb8c74e9759ccd53448b8a4120ce19cd2a
josephinehemingway <josephine.hemingway@gmail.com>
2020-11-25 17:32:46 +0800

    edit access period javadoc

--- CZ2002_Proj_Final/src/proj/EditAccessPeriodUI.java
+++ CZ2002_Proj_Final/src/proj/EditAccessPeriodUI.java
@@ -3,15 +3,32 @@ package proj;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Admin sub console that contains methods to edit Access Period of Schools
+ * @author DSAI/1 Group 5
+ * @version 1.0
+ * @since 2020-11-20
+ */
+
 public class EditAccessPeriodUI {
+	/**
+	 * Scanner to read admin user input
+	 */
 	public static final Scanner scan = new Scanner(System.in);
+	
+	/**
+	 * Date formatting to convert string entered by user into a date in specified format
+	 */
 	public static DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 	
+	/**
+	 * Method to allow admin to edit access period 
+	 * @param schoolListControl SchoolList control object that holds the school list and their details
+	 * @throws ParseException exception thrown if string is not converted to date successfully
+	 */
 	public static void editAccessPeriod(SchoolListCtrl schoolListControl) throws ParseException {
 		System.out.println("Please Enter School ID (enter '0' to exit): ");
 		String schoolID = scan.nextLine().toUpperCase();
